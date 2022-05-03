282fce0fc79233e3053d651bcd2fc06a6e2bd3d9
romitsutariya <romitsutariya@gmail.com>
2022-05-03 11:25:22 +0530

    separated provider configuration

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  profile = "default"
-  region = "ap-south-1"
-}
-
-
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket-rsutariya"
 
